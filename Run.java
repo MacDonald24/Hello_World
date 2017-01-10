b2a47697b8cefaf8f0b8d3b32a8ae8f3af103f66
MacDonald24 <macdonaldnkoana@gmail.com>
2017-01-10 11:50:46 +0200

    Add files via upload

--- Run.java
+++ Run.java
@@ -1,13 +1,11 @@
 public class Run
 {
-
-  public static void main(String[] args)
-  {
-  
-  
+	public static void main(String[] args)
+	{
+	int age = 25;
+	System.out.println(age);
+	
     System.out.println("Droppa is the life");
+	}
   
-  }
-  
-
 }
